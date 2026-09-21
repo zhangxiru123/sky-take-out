c8510344aed6c423dfcad468914720a5bf836859
zxr <2944613897@qq.com>
2026-09-21 17:04:36 +0800

    整理项目结构

---
+++ sky-server/src/main/java/com/sky/SkyServerApplication.java
@@ -0,0 +1,13 @@
+package com.sky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyServerApplication.class, args);
+    }
+
+}
